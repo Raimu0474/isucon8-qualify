63c422e35694bc705feaf52a4c3676a695fb82e3
k-murata <erufingame@gmail.com>
2019-09-05 23:30:00 +0900

    ローカルで動くように調整

--- db/init-user.sh
+++ db/init-user.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat <<'EOF' | mysql -uroot
+cat <<'EOF' | mysql --host=127.0.0.1 -uroot -P3400
 CREATE USER 'isucon'@'%' IDENTIFIED BY 'isucon';
 GRANT ALL ON torb.* TO 'isucon'@'%';
 CREATE USER 'isucon'@'localhost' IDENTIFIED BY 'isucon';
